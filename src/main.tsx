d307294701dc1b391fe8154fac6f68bfbac9c1fa
姜衿 <70458277@qq.com>
2023-02-24 11:43:08 +0800

    路由完善

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <React.StrictMode>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </React.StrictMode>,
   </Provider>
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,37 @@
 import Home from '../pages/index/index';
 import Login from '../pages/login/index';
 import User from '../pages/user/index';
-import { RouteObject, useRoutes, Navigate } from 'react-router-dom';
-const GetRouters = () => {
-    const routes: RouteObject[] = [
-        {
-            path: '/',
-            element: <Home />,
-        },
-        {
-            path: '/user',
-            element: <User />,
-        },
-        {
-            path: '/login',
-            element: <Login />,
-        }
-    ]
-    return useRoutes(routes);
+import { Navigate } from 'react-router-dom';
+import { ChromeFilled, CrownFilled, SmileFilled, TabletFilled } from '@ant-design/icons';
+import Layout from '../components/Layout';
+type routerType = {
+    path?: string,
+    element: any,
+    children?: Array<routerType>,
+    index?: boolean,
+    icon?:any,
+    name?: string,
 }
-export default GetRouters;
+const routers: routerType[] = [
+    {
+        path: '/',
+        element: <Layout />,
+        icon: <SmileFilled />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: '/user',
+                element: <User />
+            }
+        ]
+    },
+    {
+        path: '/login',
+        element: <Login />
+    },
+];
+export default routers;
 
