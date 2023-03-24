55999c70cd29292356304884fcdb4ace26999bc2
姜衿 <70458277@qq.com>
2023-03-24 14:03:14 +0800

    代码优化。

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
-
-  const handleSubmit = (event:any) => {
-    event.preventDefault();
-    console.log("Form submitted with values: ", {
-      username,
-      password,
-      rememberMe,
-    });
-  };
 
   return (
-    <div>
-        <div className="shadow-[0_2px_10px_0_rgb(25_93_194_/_7%)]
-bg-white border border-[#e9eaf3] rounded-[30px] w-auto inline-block flex flex-col items-center md:justify-center gap-4 md:!w-[600px] p-[20px_36px]">
-  <div className="mb-1 text-2xl font-bold text-center">欢迎回来</div>
-</div>
+    <div className="flex flex-col items-center justify-center w-screen px-2 py-8 md:h-screen bg-dashboard-neutral-200 dark:bg-gray-800 dark:text-white md:py-0 md:px-0">
+      <div className="shadow-[0_2px_10px_0_rgb(25_93_194_/_7%)] bg-white border border-[#e9eaf3] rounded-[30px] w-auto inline-block flex flex-col items-center md:justify-center gap-4 md:!w-[600px] p-[20px_36px]">
+        <div className="mb-1 text-2xl font-bold text-center">欢迎回来</div>
+        <div className="mb-1 text-sm text-center text-dashboard-neutral-600">Next 官网管理后台 </div>
+        <div className="relative w-full">
+          <div className="flex flex-col gap-2.5 w-full">
+            <input className="p-4 text-sm transition-all duration-300 ease-in-out bg-white border border-gray-300 outline-none placeholder:text-dashboard-neutral-500 ring-0 shadow-[0_2px_12px_0_rgb(11_22_44_/_5%)] rounded-[52px] hover:border-dashboard-neutral-500 hover:border hover:shadow-[0_2px_12px_0_rgb(11_22_44_/_10%)] focus:border-dashboard-primary focus:text-dashboard-neutral-800 " type="password" id="private_key" name="private_key" placeholder="请输入您的账号" />
+          </div>
+          <div className="mt-5 flex flex-col gap-2.5 w-full">
+            <input className="p-4 text-sm transition-all duration-300 ease-in-out bg-white border border-gray-300 outline-none placeholder:text-dashboard-neutral-500 ring-0 shadow-[0_2px_12px_0_rgb(11_22_44_/_5%)] rounded-[52px] hover:border-dashboard-neutral-500 hover:border hover:shadow-[0_2px_12px_0_rgb(11_22_44_/_10%)] focus:border-dashboard-primary focus:text-dashboard-neutral-800 " type="password" id="private_key" name="private_key" placeholder="请输入您的钱包地址" />
+          </div>
+          <button className="mt-5 rounded-full shadow-[0_2px_10px_0_rgb(25_93_194_/_7%)] border text-white border-[#e9eaf3] hover:scale-110 px-6 py-4 text-md bg-[#1476ff] hover:bg-[#004cff] flex justify-center items-center transition-all duration-300 ease-in-out flex justify-center w-full gap-2">
+            登陆
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
