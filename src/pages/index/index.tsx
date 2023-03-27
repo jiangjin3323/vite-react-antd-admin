f6ece94ea2109409467a5c5ee01d6cd7766b8f52
姜衿 <70458277@qq.com>
2023-03-27 16:45:19 +0800

    banner增删改查结束。

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Table, Button, Image } from 'antd';
+import React, { useState, useEffect, Key } from 'react';
+import { Table, Button, Image, Modal, Form, Input, message } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
-import { getBannerListApi } from '../../utils/request/api';
+import { getBannerListApi, addBannerApi, deleteBannerApi, updateBannerApi } from '../../utils/request/api';
 interface DataType {
     key: React.Key;
     id: number,
@@ -10,44 +10,63 @@ interface DataType {
     updataTime: Date,
     createTime: Date,
 }
-
-const columns: ColumnsType<DataType> = [
-    {
-        title: 'ID',
-        width: 100,
-        dataIndex: 'id',
-        key: 'id',
-    },
-    {
-        title: '图片',
-        width: 100,
-        dataIndex: 'url',
-        key: 'url',
-        render: (url) => <Image src={url} width={70} height={50} />,
-    },
-    {
-        title: '跳转地址',
-        dataIndex: 'linkUrl',
-        key: 'linkUrL',
-        width: 150,
-    },
-    {
-        title: '更新时间',
-        dataIndex: 'updataTime',
-        key: 'updataTime',
-        width: 150,
-    },
-    {
-        title: '创建时间',
-        dataIndex: 'createTime',
-        key: 'createTime',
-        width: 150,
-    }
-];
-
+interface ModalProps {
+    visible: boolean;
+    onCancel: () => void;
+    onSubmit: (values: any) => void;
+}
 
 const App: React.FC = () => {
-    let [data, setData]: [data: DataType[], setData: any] = useState([]);
+    const columns: ColumnsType<DataType> = [
+        {
+            title: 'ID',
+            width: 100,
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: '图片',
+            width: 100,
+            dataIndex: 'url',
+            key: 'url',
+            render: (url: string) => <Image src={url} width={70} height={50} />,
+        },
+        {
+            title: '跳转地址',
+            dataIndex: 'linkUrl',
+            key: 'linkUrL',
+            width: 150,
+        },
+        {
+            title: '更新时间',
+            dataIndex: 'updataTime',
+            key: 'updataTime',
+            width: 150,
+        },
+        {
+            title: '创建时间',
+            dataIndex: 'createTime',
+            key: 'createTime',
+            width: 150,
+        },
+        {
+            title: '编辑',
+            dataIndex: 'id',
+            key: 'id',
+            width: 50,
+            fixed: 'right',
+            render: (id: number) => <Button className='bg-[#1677ff]' key="submit" type="primary" onClick={() => { updateBtnClick(id) }}> 操作 </Button>
+        }
+    ];
+
+    const [form] = Form.useForm();
+    const [messageApi, contextHolder] = message.useMessage();
+    let [data, setData] = useState<DataType[]>([]);
+    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [isAddPopup, setIsAddPopup] = useState<boolean>(false);
+    const [confirmLoading, setConfirmLoading] = useState<boolean>(false);
+    const [updateId, setUpdateId] = useState<number>(0);
     const getBannerListFunc = async () => {
         const [err, res]: any = await getBannerListApi();
         if (err) return;
@@ -59,14 +78,15 @@ const App: React.FC = () => {
         })
         setData(list);
     }
-    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-    const [loading, setLoading] = useState(false);
-    const start = () => {
+
+    const deleteBtnClick = async () => {
         setLoading(true);
-        setTimeout(() => {
-            setSelectedRowKeys([]);
-            setLoading(false);
-        }, 1000);
+        for (let i = 0; i < selectedRowKeys.length; i++) {
+            const [err, res]: any = await deleteBannerApi({ id: selectedRowKeys[i] })
+        }
+        setSelectedRowKeys([]);
+        setLoading(false);
+        getBannerListFunc();
     };
     const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
         setSelectedRowKeys(newSelectedRowKeys);
@@ -80,14 +100,77 @@ const App: React.FC = () => {
 
     useEffect(() => {
         getBannerListFunc();
-    }, [data])
+    }, [])
+
+    const addPopupOkFun = async (id?: number) => {
+        setConfirmLoading(true);
+        const values = await form.validateFields();
+        if (!id) {
+            const [err, res]: any = await addBannerApi(values);
+            if (err) return messageApi.error(err.msg);
+        } else {
+            const [err, res]: any = await updateBannerApi({
+                ...values,
+                id
+            });
+            if (err) return messageApi.error(err.msg);
+        }
+        setConfirmLoading(false);
+        setIsAddPopup(false);
+        setUpdateId(0);
+        form.resetFields();
+        messageApi.success('ok');
+        getBannerListFunc();
+    }
+    const addPopupCancelFun = async () => {
+        setIsAddPopup(false);
+        setUpdateId(0)
+    }
+    const updateBtnClick = async (id: number) => {
+        setUpdateId(id)
+        setIsAddPopup(true);
+    }
     return (
         <>
+            {contextHolder}
+            <Modal
+                open={isAddPopup}
+                onCancel={addPopupCancelFun}
+                footer={[
+                    <Button key="back" onClick={addPopupCancelFun}>
+                        取消
+                    </Button>,
+                    <Button className='bg-[#1677ff]' key="submit" type="primary" loading={confirmLoading} onClick={() => { addPopupOkFun(updateId ? updateId : 0) }}>
+                        确定
+                    </Button>,
+                ]}
+            >
+                <div className='mt-6'></div>
+                <Form
+                    form={form}
+
+                >
+                    <Form.Item
+                        label="图片地址"
+                        name="url"
+                        rules={[{ required: true, message: '请填写图片地址' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="跳转地址（选填）"
+                        name="linkUrl"
+                    >
+                        <Input />
+                    </Form.Item>
+                </Form>
+
+            </Modal>
             <div style={{ marginBottom: 16 }}>
-                <Button className='bg-[#1677ff] mr-5' type="primary">
+                <Button className='bg-[#1677ff] mr-5' type="primary" onClick={() => { setIsAddPopup(!isAddPopup) }}>
                     新增
                 </Button>
-                <Button className='bg-[#1677ff]' type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
+                <Button className='bg-[#1677ff]' type="primary" onClick={deleteBtnClick} disabled={!hasSelected} loading={loading}>
                     删除
                 </Button>
                 <span style={{ marginLeft: 8 }}>
--- src/utils/request/api.ts
+++ src/utils/request/api.ts
@@ -1,3 +1,6 @@
 import axios from "../request/request";
 export const loginApi = (data: any = {}) => axios.post(`login`, data);
-export const getBannerListApi = (data: any = {}) => axios.get(`banner`);
+export const getBannerListApi = () => axios.get(`banner`);
+export const addBannerApi = (data: any = {}) => axios.post('banner', data);
+export const deleteBannerApi = (data: any = {}) => axios.post('banner/delete', data);
+export const updateBannerApi = (data: any = {}) => axios.post('banner/update', data);
