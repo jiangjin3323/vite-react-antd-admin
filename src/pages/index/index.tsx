8f5a6908e546eeee672c7d916532dbeb1686e624
姜衿 <70458277@qq.com>
2023-03-27 14:33:27 +0800

    新增banner列表

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,45 +1,104 @@
-import React from 'react';
-import { Carousel } from 'antd';
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
-const contentStyle: React.CSSProperties = {
-    margin: 0,
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-};
+import React, { useState, useEffect } from 'react';
+import { Table, Button, Image } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+import { getBannerListApi } from '../../utils/request/api';
+interface DataType {
+    key: React.Key;
+    id: number,
+    url: string,
+    linkUrl: string,
+    updataTime: Date,
+    createTime: Date,
+}
+
+const columns: ColumnsType<DataType> = [
+    {
+        title: 'ID',
+        width: 100,
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: '图片',
+        width: 100,
+        dataIndex: 'url',
+        key: 'url',
+        render: (url) => <Image src={url} width={70} height={50} />,
+    },
+    {
+        title: '跳转地址',
+        dataIndex: 'linkUrl',
+        key: 'linkUrL',
+        width: 150,
+    },
+    {
+        title: '更新时间',
+        dataIndex: 'updataTime',
+        key: 'updataTime',
+        width: 150,
+    },
+    {
+        title: '创建时间',
+        dataIndex: 'createTime',
+        key: 'createTime',
+        width: 150,
+    }
+];
+
 
 const App: React.FC = () => {
-    //全局store
-    const store = useSelector(state => state);
-    const onChange = (currentSlide: number) => {
-        console.log(currentSlide);
+    let [data, setData]: [data: DataType[], setData: any] = useState([]);
+    const getBannerListFunc = async () => {
+        const [err, res]: any = await getBannerListApi();
+        if (err) return;
+        const list: DataType[] = res.data.map((item: DataType) => {
+            return {
+                ...item,
+                key: item.id
+            }
+        })
+        setData(list);
+    }
+    const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+    const [loading, setLoading] = useState(false);
+    const start = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setSelectedRowKeys([]);
+            setLoading(false);
+        }, 1000);
+    };
+    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
+        setSelectedRowKeys(newSelectedRowKeys);
     };
 
+    const rowSelection = {
+        selectedRowKeys,
+        onChange: onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0;
+
+    useEffect(() => {
+        getBannerListFunc();
+    }, [data])
     return (
-        <div style={{ width: '100%' }}>
-            <Carousel afterChange={onChange}>
-                <Link to='/user'>
-                    <div>
-                        <h3 style={contentStyle}>1</h3>
-                    </div>
-                </Link>
-                <Link to='/login'>
-                    <div>
-                        <h3 style={contentStyle}>2</h3>
-                    </div>
-                </Link>
-                <div>
-                    <h3 style={contentStyle}>3</h3>
-                </div>
-                <div>
-                    <h3 style={contentStyle}>4</h3>
-                </div>
-            </Carousel>
-        </div>
-    );
-};
+        <>
+            <div style={{ marginBottom: 16 }}>
+                <Button className='bg-[#1677ff] mr-5' type="primary">
+                    新增
+                </Button>
+                <Button className='bg-[#1677ff]' type="primary" onClick={start} disabled={!hasSelected} loading={loading}>
+                    删除
+                </Button>
+                <span style={{ marginLeft: 8 }}>
+                    {hasSelected ? `已选择 ${selectedRowKeys.length} 条` : ''}
+                </span>
+
+            </div>
+            <Table rowSelection={rowSelection} columns={columns} dataSource={data} scroll={{ x: 1500, y: 480 }} />
+
+        </>
+    )
+}
 
 export default App;
--- src/utils/request/api.ts
+++ src/utils/request/api.ts
@@ -1,2 +1,3 @@
 import axios from "../request/request";
 export const loginApi = (data: any = {}) => axios.post(`login`, data);
+export const getBannerListApi = (data: any = {}) => axios.get(`banner`);
